32533e3e3a63a79e0ed01d6fa201aa4e248b88f3
Alex Demars <alex.demars@insa-cvl.fr>
2021-10-22 17:25:30 -0400

    PageRank fonctionnel entièrement

--- Devoir_1/spark_project/src/main/scala/PageRank.scala
+++ Devoir_1/spark_project/src/main/scala/PageRank.scala
@@ -18,31 +18,35 @@ object PageRank {
   val sc = new SparkContext(conf)
   sc.setLogLevel("ERROR")
 
-  val iter = 20
+  val iters = 20
   val dumpFactor = 0.85
 
-  val pairs = sc.textFile("./urldata.txt").map { s =>
-    val urls = s.split("\\s+") // Splits a line into an array of 2 elements according space(s)
-    (urls(0), urls(1))
-  }
-
-  val links = pairs.distinct().groupByKey().cache() // RDD1 <string, string> -> RDD2<string, iterable>
-  val urlRank = links.mapValues(v => 1.0)
-
-  for (i <- 1 to iters) {
-    println("Iteration : "+i)
-    val contribs = links.join(ranks).values.flatMap { case (urls, rank) =>
-      val size = urls.size
-      urls.map(url => (url, rank / size))
+  def main(args: Array[String]): Unit = {
+    val pairs: RDD[(String, String)] = sc.textFile("./src/main/scala/urldata.txt").map { s =>
+      val urls = s.split("\\s+") // Splits a line into an array of 2 elements according space(s)
+      (urls(0), urls(1))
     }
 
-    urlRank = contribs.reduceByKey(_ + _).mapValues(x => (1 - dumpFactor) + dumpFactor * x)
-    urlRank.take(10).foreach(println)
+    val links: RDD[(String, Iterable[String])] = pairs.distinct().groupByKey().cache() // RDD1 <string, string> -> RDD2<string, iterable>
+    var urlRank: RDD[(String, Double)] = links.mapValues(v => 1.0)
+
+    for (i <- 1 to iters) {
+      println("Iteration : "+i)
+      val contribs = links.join(urlRank).values.flatMap { case (urls, rank) =>
+        val size = urls.size
+        urls.map(url => (url, rank / size))
+      }
+
+      val newUrlRanks: RDD[(String, Double)] = contribs.reduceByKey(_ + _).mapValues(x => (1 - dumpFactor) + dumpFactor * x)
+      urlRank = newUrlRanks.rightOuterJoin(urlRank).mapValues(elem => if (elem._1.isEmpty) 1 - dumpFactor else elem._1.get)
+      urlRank.take(10).foreach(println)
+    }
+
+    urlRank.sortBy(_._2, ascending = false)
+
+    // Print elements (not as file because Hadoop not installed)
+    urlRank.foreach(elem => println(elem._1 + "->" + elem._2))
+
+    sc.stop()
   }
-
-  urlRank.sortBy(_._2, false)
-  urlRank.saveAsTextFile("result")
-
-  spark.stop()
-
 }
